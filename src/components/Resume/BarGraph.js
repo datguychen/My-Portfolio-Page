c6a7249b58f093a17036b45322482448c1bcd127
datguychen <adam.tomasz.swiderski@gmail.com>
2023-08-23 16:43:49 +0200

    Resume-BarGraph-datatestid

--- src/components/Resume/BarGraph.js
+++ src/components/Resume/BarGraph.js
@@ -1,9 +1,9 @@
 export default function BarGraph({ name, percent }) {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col" data-testid="SkillsInfo">
       <div className="flex flex-row justify-between w-3/4 px-6 text-lg " data-testid="SkillsName">
         <h2>{name}</h2>
-        <h2>{percent}%</h2>
+        <h3>{percent}%</h3>
       </div>
       <div
         class="w-3/4 bg-neutral-200 h-2.5
