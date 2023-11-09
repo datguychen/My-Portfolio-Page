4b00288d2204669a68ca6079facc779b7080e07c
datguychen <adam.tomasz.swiderski@gmail.com>
2023-11-09 20:57:40 +0100

    dependencies cache v2

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,9 +1,5 @@
 import { defineConfig, devices } from '@playwright/test';
 
-/**
- * @see https://playwright.dev/docs/test-configuration
- * @type {import('@playwright/test').PlaywrightTestConfig}
- */
 export default defineConfig({
   testDir: './tests',
   timeout: 40 * 1000,
@@ -36,81 +32,19 @@ export default defineConfig({
 
   projects: [
     {
-      name: 'Chrome',
+      name: 'Chrome_With_Media',
+      testDir: './tests',
       use: {
+        launchOptions: { slowMo: 500 },
+        actionTimeout: 0,
         browserName: 'chromium',
+        headless: true,
+        screenshot: 'only-on-failure',
+        trace: 'retain-on-failure',
         ...devices['Desktop Chrome'],
         viewport: { width: 1750, height: 900 },
+        channel: 'chrome',
       },
     },
-
-    // {
-    //   name: 'Safari',
-    //   use: {
-    //     browserName: 'webkit',
-    //     ...devices['Desktop Safari'],
-    //     viewport: { width: 1800, height: 900 },
-    //   },
-    // },
-
-    // {
-    //   name: 'ChromeWithMedia',
-    //   use: {
-    //     channel: 'chrome',
-    //     viewport: { width: 1800, height: 900 },
-    //   },
-    // },
-
-   /* {
-      name: 'firefox',
-      use: {
-        ...devices['Desktop Firefox'],
-      },
-    },
-
-    {
-      name: 'webkit',
-      use: {
-        ...devices['Desktop Safari'],
-      },
-    },
-    */
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: {
-    //     ...devices['Pixel 5'],
-    //   },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: {
-    //     ...devices['iPhone 12'],
-    //   },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: {
-    //     channel: 'msedge',
-    //   },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: {
-    //     channel: 'chrome',
-    //   },
-    // },
   ],
-
-  /* Folder for test artifacts such as screenshots, videos, traces, etc. */
-  // outputDir: 'test-results/',
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   port: 3000,
-  // },
 });
